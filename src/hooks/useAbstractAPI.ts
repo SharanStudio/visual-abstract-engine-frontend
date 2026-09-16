528e6b84cfe5e07d715dc8a8ac9726ada00fe290
SharanStudio <141206291+SharanStudio@users.noreply.github.com>
2026-09-16 20:47:14 +0530

    Update useAbstractAPI.ts

--- src/hooks/useAbstractAPI.ts
+++ src/hooks/useAbstractAPI.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendURL = import.meta.env.VITE_BACKEND_URL;
+const backendURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001';
 
 export const useAbstractAPI = () => {
   const generateAbstract = async (data: any) => {
